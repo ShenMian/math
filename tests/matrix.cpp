e5479f963fa6ef0b575da710279998866fb09762
ShenMian <sms_school@outlook.com>
2021-10-13 13:51:41 +0800

    Update

--- tests/matrix.cpp
+++ tests/matrix.cpp
@@ -4,12 +4,8 @@
 #include "gtest/gtest.h"
 #include "Matrix.hpp"
 
-#include "Quaternion.hpp"
-
 TEST(Matrix, trace)
 {
     EXPECT_FLOAT_EQ(Matrix4().trace(), 4);
     EXPECT_FLOAT_EQ(Matrix3().trace(), 3);
-
-    auto t = Matrix4::createTranslate({4, 5, 6});
 }
