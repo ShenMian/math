3baf230b24d2543ad96784ee5d68c100d8af06ff
ShenMian <sms_school@outlook.com>
2021-10-27 07:44:01 +0800

    Update Matrix

--- include/Math/Matrix.hpp
+++ include/Math/Matrix.hpp
@@ -9,7 +9,7 @@
 /**
  * @brief 矩阵.
  */
-template <typename T, size_t R, size_t C>
+template <size_t R, size_t C, typename T>
 class MatrixT
 {
 public:
@@ -33,7 +33,7 @@ public:
     /**
      * @brief 获取逆矩阵.
      */
-    MatrixT<T, C, R> inversed() const;
+    MatrixT<C, R, T> inversed() const;
 
     /**
      * @brief 转置.
@@ -43,7 +43,7 @@ public:
     /**
      * @brief 获取转置后的矩阵.
      */
-    MatrixT<T, C, R> transposed() const;
+    MatrixT<C, R, T> transposed() const;
 
     /**
      * @brief 计算行列式的值.
@@ -75,7 +75,7 @@ public:
     const T* operator[](size_t row) const;
 
     template <typename T0>
-    operator MatrixT<T0, R, C>()
+    operator MatrixT<R, C, T0>()
     {
         MatrixT result;
         for(size_t r = 0; r < R; r++)
@@ -105,7 +105,7 @@ private:
 #include "Matrix.inl"
 
 #define DEF_MATRIX_NxN(n)                                        \
-    template <typename T> using Matrix##n##T = MatrixT<T, n, n>; \
+    template <typename T> using Matrix##n##T = MatrixT<n, n, T>; \
     using Matrix##n     = Matrix##n##T<float>;                   \
     using Matrix##n##f  = Matrix##n##T<float>;                   \
     using Matrix##n##d  = Matrix##n##T<double>;                  \
