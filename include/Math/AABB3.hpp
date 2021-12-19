7f03426f1a61f2c00d1732d74e1df8a9365e170a
ShenMian <sms_school@outlook.com>
2021-12-19 19:25:46 +0800

    Add AABB

---
+++ include/Math/AABB3.hpp
@@ -0,0 +1,79 @@
+﻿// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#pragma once
+
+#include "Vector3.hpp"
+
+class AABB3
+{
+public:
+	Vector3 min;
+	Vector3 max;
+
+	AABB3(const Vector3& min, const Vector3& max);
+
+	/**
+	 * @brief 拓展到包含指定点.
+	 *
+	 * @param point 指定点.
+	 */
+	void expand(const Vector3& point);
+
+	/**
+	 * @brief 拓展到包含指定 AABB.
+	 *
+	 * @param point AABB.
+	 */
+	void expand(const AABB3& aabb);
+
+	/**
+	 * @brief 判断是否包含指定点.
+	 *
+	 * @param point  判断是否包含的点.
+	 * @return true  包含.
+	 * @return false 不包含.
+	 */
+	bool contains(const Vector3& point) const;
+
+	/**
+	 * @brief 判断是否包含指定 AABB.
+	 *
+	 * @param aabb   判断是否包含的 AABB.
+	 * @return true  包含.
+	 * @return false 不包含.
+	 */
+	bool contains(const AABB3& aabb) const;
+
+	/**
+	 * @brief 判断是否与 AABB 相交.
+	 *
+	 * @param  aabb  判断是否相交的 AABB.
+	 * @return true  相交.
+	 * @return false 不相交.
+	 */
+	bool intersects(const AABB3& aabb) const;
+
+	/**
+	 * @brief 获取几何中心.
+	 */
+	Vector3 getCenter() const;
+
+	/**
+	 * @brief 判断是否有效.
+	 *
+	 * @return true  有效.
+	 * @return false 无效.
+	 */
+	bool isValid() const;
+
+	/**
+	 * @brief 判断大小是否为空.
+	 *
+	 * @return true  空.
+	 * @return false 非空.
+	 */
+	bool isEmpty() const;
+};
+
+#include "AABB3.inl"
---
+++ include/Math/AABB3.inl
@@ -0,0 +1,59 @@
+﻿// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#include <cassert>
+
+inline AABB3::AABB3(const Vector3& min, const Vector3& max)
+	: min(min), max(max)
+{
+}
+
+inline void AABB3::expand(const Vector3& point)
+{
+	min.x = std::min(min.x, point.x);
+	min.y = std::min(min.y, point.y);
+	min.z = std::min(min.z, point.z);
+	max.x = std::max(max.x, point.x);
+	max.y = std::max(max.y, point.y);
+	max.z = std::max(max.z, point.z);
+}
+
+inline void AABB3::expand(const AABB3& aabb)
+{
+	assert(aabb.isValid());
+	expand(aabb.min);
+	expand(aabb.max);
+}
+
+inline bool AABB3::contains(const Vector3& point) const
+{
+	assert(isValid());
+	return false;
+}
+
+inline bool AABB3::contains(const AABB3& aabb) const
+{
+	assert(isValid() && aabb.isValid());
+	return false;
+}
+
+inline bool AABB3::intersects(const AABB3& aabb) const
+{
+	assert(isValid() && aabb.isValid());
+	return false;
+}
+
+inline Vector3 AABB3::getCenter() const
+{
+	return (min + max) / 2.f;
+}
+
+inline bool AABB3::isValid() const
+{
+	return min.x <= max.x && min.y <= max.y && min.z <= max.z;
+}
+
+inline bool AABB3::isEmpty() const
+{
+	return min == max;
+}
