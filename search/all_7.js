9bff5912bc623ca2ba4858855b8ff38c8622a852
ShenMian <ShenMian@users.noreply.github.com>
2022-02-15 09:05:01 +0000

    deploy: 4fa6eff323cf55893957d83a06077cbbf3944624

--- search/all_7.js
+++ search/all_7.js
@@ -4,7 +4,7 @@ var searchData=
   ['matrixt_1',['MatrixT',['../class_matrix_t.html',1,'MatrixT&lt; T, R, C &gt;'],['../class_matrix_t.html#ac6ddf9beb0c909bbad0245e8d97c27f2',1,'MatrixT::MatrixT()'],['../class_matrix_t.html#ad2db3d095d584e7f7aad452f6662352c',1,'MatrixT::MatrixT(const std::initializer_list&lt; T &gt; &amp;list)']]],
   ['matrixt_3c_20t_2c_20n_2c_20n_20_3e_2',['MatrixT&lt; T, N, N &gt;',['../class_matrix_t_3_01_t_00_01_n_00_01_n_01_4.html',1,'']]],
   ['max_3',['max',['../class_a_a_b_b3.html#aedf9fc44f621ee19077dd9220eb824e6',1,'AABB3']]],
-  ['maxcoeff_4',['maxCoeff',['../class_matrix_t.html#a505c22afd4a8c0f4d9b54a40ae047f16',1,'MatrixT']]],
+  ['maxcoeff_4',['maxCoeff',['../class_matrix_t.html#a505c22afd4a8c0f4d9b54a40ae047f16',1,'MatrixT::maxCoeff()'],['../class_vector_t.html#a2febc11578aee414c172d6b8209533db',1,'VectorT::maxCoeff()']]],
   ['min_5',['min',['../class_a_a_b_b3.html#a0d0a92e0abcd498bdbeb328fad19ea05',1,'AABB3']]],
-  ['mincoeff_6',['minCoeff',['../class_matrix_t.html#accb0971d1e5d93efc5cc19ad6e8c406d',1,'MatrixT']]]
+  ['mincoeff_6',['minCoeff',['../class_matrix_t.html#accb0971d1e5d93efc5cc19ad6e8c406d',1,'MatrixT::minCoeff()'],['../class_vector_t.html#a97fd408425e1ae1bab16214224631e4a',1,'VectorT::minCoeff()']]]
 ];
