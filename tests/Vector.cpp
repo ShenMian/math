9f34c02d02e0adf67a219f25a2787ffbe8cf26f6
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-04-06 15:29:24 +0800

    Update Vector.cpp

--- tests/Vector.cpp
+++ tests/Vector.cpp
@@ -28,6 +28,7 @@ TEST(Vector, normalize)
 {
 	EXPECT_FLOAT_EQ(Vectorf<5>({1.f, 2.f, 3.f, 4.f, 5.f}).normalize().norm(), 1.f);
 	EXPECT_EQ(Vectorf<5>({2.f, 3.f, 4.f, 5.f, 6.f}).normalize(), Vectorf<5>({4.f, 6.f, 8.f, 10.f, 12.f}).normalize());
+    EXPECT_EQ(Vectorf<5>(0.f).normalize(), Vectorf<5>(0.f));
 }
 
 TEST(Vector, dot)
