e5a8b218a5f220009f40c299839cc10a9ff830eb
ShenMian <ShenMian@users.noreply.github.com>
2022-04-11 01:42:47 +0000

    deploy: e64ef689344d81260bce5eef5f3242fc43cbaf29

--- search/all_a.js
+++ search/all_a.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['quaterniont_0',['QuaternionT',['../class_quaternion_t.html',1,'QuaternionT&lt; T &gt;'],['../class_quaternion_t.html#aa7463b42972c45dd99f96ebf121766eb',1,'QuaternionT::QuaternionT()=default'],['../class_quaternion_t.html#af0ba22737f08bc185577933d061a9742',1,'QuaternionT::QuaternionT(T x, T y, T z, T w)'],['../class_quaternion_t.html#a590d8abadff8f3ccf3784d9c349c8052',1,'QuaternionT::QuaternionT(const Matrix4T&lt; T &gt; &amp;mat)']]]
+  ['quaterniont_0',['QuaternionT',['../class_quaternion_t.html',1,'QuaternionT&lt; T &gt;'],['../class_quaternion_t.html#aa7463b42972c45dd99f96ebf121766eb',1,'QuaternionT::QuaternionT()=default'],['../class_quaternion_t.html#af0ba22737f08bc185577933d061a9742',1,'QuaternionT::QuaternionT(T x, T y, T z, T w)'],['../class_quaternion_t.html#a590d8abadff8f3ccf3784d9c349c8052',1,'QuaternionT::QuaternionT(const Matrix4T&lt; T &gt; &amp;mat)']]],
+  ['quaterniont_3c_20float_20_3e_1',['QuaternionT&lt; float &gt;',['../class_quaternion_t.html',1,'']]]
 ];
--- search/classes_3.js
+++ search/classes_3.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['quaterniont_0',['QuaternionT',['../class_quaternion_t.html',1,'']]]
+  ['quaterniont_0',['QuaternionT',['../class_quaternion_t.html',1,'']]],
+  ['quaterniont_3c_20float_20_3e_1',['QuaternionT&lt; float &gt;',['../class_quaternion_t.html',1,'']]]
 ];
