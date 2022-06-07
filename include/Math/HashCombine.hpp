6b04057ca261e20ffc965e55d74d301ce8358b98
ShenMian <70068015+ShenMian@users.noreply.github.com>
2022-06-07 16:34:04 +0800

    Add HashCombine.hpp

---
+++ include/Math/HashCombine.hpp
@@ -0,0 +1,32 @@
+﻿// Copyright 2021 SMS
+// License(Apache-2.0)
+
+#pragma once
+
+#include <cstddef>
+#include <functional>
+
+template <typename T>
+inline void hash_combine(std::size_t& seed, const T& v)
+{
+	seed ^= std::hash<T>{}(v)+0x9e3779b9 + (seed << 6) + (seed >> 2);
+}
+
+template <typename T, typename... Ts>
+inline void hash_combine(std::size_t& seed, const T& v, Ts... rest)
+{
+	hash_combine(seed, v);
+	if constexpr (sizeof...(Ts) > 1)
+		hash_combine(seed, rest...);
+}
+
+#define MAKE_HASHABLE(type, ...)                          \
+    namespace std {                                       \
+        template<> struct hash<type> {                    \
+            std::size_t operator()(const type &t) const { \
+                std::size_t ret = 0;                      \
+                hash_combine(ret, __VA_ARGS__);           \
+                return ret;                               \
+            }                                             \
+        };                                                \
+    }
