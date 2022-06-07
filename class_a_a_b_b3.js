ac637b5d54f768276f40ccc2b541c48c406c2ec3
ShenMian <ShenMian@users.noreply.github.com>
2022-06-07 09:24:33 +0000

    deploy: 8b73c4f4394ce30626ea1d191183f9b462c8e881

--- class_a_a_b_b3.js
+++ class_a_a_b_b3.js
@@ -11,6 +11,7 @@ var class_a_a_b_b3 =
     [ "expand", "class_a_a_b_b3.html#aa90bae76456f16437022d649cf021ed2", null ],
     [ "intersects", "class_a_a_b_b3.html#a85fdc49986c681ccaedf1f83237313a7", null ],
     [ "valid", "class_a_a_b_b3.html#a6e931046999a22a3283e27688df796d3", null ],
+    [ "volume", "class_a_a_b_b3.html#a21a56f44496727bf82c3ea900d7b6a26", null ],
     [ "max", "class_a_a_b_b3.html#aedf9fc44f621ee19077dd9220eb824e6", null ],
     [ "min", "class_a_a_b_b3.html#a0d0a92e0abcd498bdbeb328fad19ea05", null ]
 ];
