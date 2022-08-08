ea1052b3816c33b5d77c8fa9904a58769bea3ca8
ShenMian <ShenMian@users.noreply.github.com>
2022-08-08 12:27:31 +0000

    deploy: ced3aecf87b07db9aa4545c166319eb8f3f901b3

--- class_vector_t.js
+++ class_vector_t.js
@@ -1,6 +1,6 @@
 var class_vector_t =
 [
-    [ "VectorT", "class_vector_t.html#a343a9320f718caa0e157ed4c37ad3c6b", null ],
+    [ "VectorT", "class_vector_t.html#a5e0dbe768283a0e39b190346fe212844", null ],
     [ "VectorT", "class_vector_t.html#aa71f732e9e7689b0172afc15ff4ab95b", null ],
     [ "VectorT", "class_vector_t.html#a2a01fd218f8531cf5912e148d82efcbc", null ],
     [ "VectorT", "class_vector_t.html#a6a1c91e16c228869f76e8aad050911d0", null ],
@@ -11,9 +11,9 @@ var class_vector_t =
     [ "dot", "class_vector_t.html#a7711de2a83cdda3ed97c7d7bd666c25e", null ],
     [ "maxCoeff", "class_vector_t.html#a66b15f4c6ccf864ec5c9bf67d3720230", null ],
     [ "minCoeff", "class_vector_t.html#aa598ef45c9dbe8b4180ff41a367167b5", null ],
-    [ "norm", "class_vector_t.html#a2a0ac9ff91767cb4b09a021191013e39", null ],
-    [ "normalize", "class_vector_t.html#aafae2237f0cc68af441c99c7aac5635b", null ],
-    [ "normalized", "class_vector_t.html#a6e21dcfe641727a59ec3b7ac692b91b5", null ],
+    [ "norm", "class_vector_t.html#a2c549a8685b703417286411d9fd586a0", null ],
+    [ "normalize", "class_vector_t.html#abc0be33586a59fe9d1630a48b3b0d429", null ],
+    [ "normalized", "class_vector_t.html#a1eec617fb10475efe2a2d69a47f38f8f", null ],
     [ "normSq", "class_vector_t.html#a7dfeb6d8732fb75c616989886c295169", null ],
     [ "sum", "class_vector_t.html#ab8134325385791d5af23788d4c9f1153", null ]
 ];
