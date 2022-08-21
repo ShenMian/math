4db7aaddfebb8e90e390b5a57544713f033a3093
ShenMian <ShenMian@users.noreply.github.com>
2022-08-21 13:40:16 +0000

    deploy: bb6174107c5f4f766901c06054dceaf71a592641

--- class_quaternion_t.js
+++ class_quaternion_t.js
@@ -3,11 +3,13 @@ var class_quaternion_t =
     [ "QuaternionT", "class_quaternion_t.html#aa7463b42972c45dd99f96ebf121766eb", null ],
     [ "QuaternionT", "class_quaternion_t.html#a69b5157e3d9b3bbf9c4abdefc3eb46d2", null ],
     [ "QuaternionT", "class_quaternion_t.html#a93fa3d39201786f122e27f5bbc34a9d1", null ],
+    [ "conjugate", "class_quaternion_t.html#a46e9c05d998bb7267b0ceef73b32cc75", null ],
     [ "eular", "class_quaternion_t.html#a75cb8bbd9feebe6c8a77163c79027018", null ],
     [ "eular", "class_quaternion_t.html#a9d56de9e17a4342fda25a3146ab3839c", null ],
     [ "inverse", "class_quaternion_t.html#a29257644768362cb52f26f3314cd297f", null ],
     [ "inversed", "class_quaternion_t.html#aa0d053a351499f5d6737e462849da5b1", null ],
     [ "normalize", "class_quaternion_t.html#a1cb09eeaa680ba17de322045044c6169", null ],
+    [ "normalized", "class_quaternion_t.html#a70073ccf159899bcc15a3eb1c4e38c5f", null ],
     [ "size", "class_quaternion_t.html#aaefcb88a03abac68d8ba034603449a7c", null ],
     [ "sizeSq", "class_quaternion_t.html#af1031a294f20fc4b29b820ac2dd33181", null ]
 ];
