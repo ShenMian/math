b2ef93a2ec41a5583a3a25740a951f334bade163
ShenMian <ShenMian@users.noreply.github.com>
2022-10-28 14:16:59 +0000

    deploy: fed4c4bc683c8df1a7abe504708a854aa3c548e9

--- search/all_1.js
+++ search/all_1.js
@@ -1,7 +1,7 @@
 var searchData=
 [
   ['center_0',['center',['../class_a_a_b_b3.html#a43b2f9010471a3e2e5fd21735e13efca',1,'AABB3']]],
-  ['clamp_1',['clamp',['../class_vector_t.html#af30ae6ce8330f149cc98d1b287f94843',1,'VectorT']]],
+  ['clamp_1',['clamp',['../class_vector_t.html#ad3c0312d730a36aa46ba2d70bbc28428',1,'VectorT']]],
   ['clear_2',['clear',['../class_a_a_b_b3.html#a0b01ae75380efb656c1a246d9b21b2ae',1,'AABB3']]],
   ['col_3',['col',['../class_matrix_t.html#a5b0619a5380497cebb7eb8fc1e424d22',1,'MatrixT']]],
   ['cols_4',['cols',['../class_matrix_t.html#a31aec19b7a0d0a989abbcb7a5185f450',1,'MatrixT']]],
@@ -14,5 +14,5 @@ var searchData=
   ['createrotationz_11',['createRotationZ',['../class_matrix_t.html#aed21161cd6a5befe184d0657c735583a',1,'MatrixT']]],
   ['createscale_12',['createScale',['../class_matrix_t.html#a578ae18298c73f56c69bc969ffd02b99',1,'MatrixT']]],
   ['createtranslate_13',['createTranslate',['../class_matrix_t.html#a5f05219db855ca60431231b4b6edbf02',1,'MatrixT']]],
-  ['cross_14',['cross',['../class_vector_t.html#a258208d4be6b7b5f3ed032d958a463fc',1,'VectorT::cross()'],['../class_vector_t_3_01_t_00_012_01_4.html#acc003cea3373111572eadf1720b46342',1,'VectorT&lt; T, 2 &gt;::cross()'],['../class_vector_t_3_01_t_00_013_01_4.html#a7407d5796d9a456c2d0b68268dbaceac',1,'VectorT&lt; T, 3 &gt;::cross()'],['../class_vector_t_3_01_t_00_014_01_4.html#ab794926bfe6f96c34963c07f3f9aa8c3',1,'VectorT&lt; T, 4 &gt;::cross()']]]
+  ['cross_14',['cross',['../class_vector_t.html#a6121eef21255c08c9f78579f5b791ccd',1,'VectorT::cross()'],['../class_vector_t_3_01_t_00_012_01_4.html#acc003cea3373111572eadf1720b46342',1,'VectorT&lt; T, 2 &gt;::cross()'],['../class_vector_t_3_01_t_00_013_01_4.html#a7407d5796d9a456c2d0b68268dbaceac',1,'VectorT&lt; T, 3 &gt;::cross()'],['../class_vector_t_3_01_t_00_014_01_4.html#ab794926bfe6f96c34963c07f3f9aa8c3',1,'VectorT&lt; T, 4 &gt;::cross()']]]
 ];
