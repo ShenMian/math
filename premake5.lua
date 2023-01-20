f1da8060da6d42e5ce6253d05a90f89f1d7f4eb6
ShenMian <sms_school@outlook.com>
2023-01-20 22:19:09 +0800

    refactor: update premake

--- premake5.lua
+++ premake5.lua
@@ -1,7 +1,7 @@
 ﻿-- Copyright 2023 ShenMian
 -- License(Apache-2.0)
 
--- include("deps/premake/solution_items.lua")
+include("deps/premake/solution_items.lua")
 
 workspace("math")
     architecture("x86_64")
@@ -10,11 +10,11 @@ workspace("math")
 
     configurations({ "Debug", "Release" })
 
-    -- solution_items({
-    --     "README.md",
-    --     "CMakeLists.txt",
-    --     "premake5.lua",
-    -- })
+    solution_items({
+        "README.md",
+        "CMakeLists.txt",
+        "premake5.lua",
+    })
 
     filter("configurations:Debug")
         defines("DEBUG")
