ecc6a90f4435b6c45e13968e2ce424404b1f1f59
ShenMian <sms_school@outlook.com>
2023-02-01 02:13:45 +0800

    refactor: update math.hpp

--- src/math/math.hpp
+++ src/math/math.hpp
@@ -3,19 +3,15 @@
 
 #pragma once
 
+#include "aabb.hpp"
+#include "easing.hpp"
+#include "matrix/matrix.hpp"
+#include "quaternion.hpp"
+#include "simd.hpp"
+#include "transform.hpp"
 #include "vector/vector.hpp"
+
 #include "vector/vector2.hpp"
 #include "vector/vector3.hpp"
 #include "vector/vector4.hpp"
-
-#include "matrix/matrix.hpp"
 // #include "matrix/matrix_nn.hpp"
-
-#include "quaternion.hpp"
-
-#include "aabb.hpp"
-#include "transform.hpp"
-
-#include "simd.hpp"
-
-#include "easing.hpp"
