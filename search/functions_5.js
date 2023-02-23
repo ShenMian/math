6e1b05553f3147de26e765474ae26894e132d54c
ShenMian <ShenMian@users.noreply.github.com>
2023-02-23 14:05:48 +0000

    deploy: 550c3b13d71b348a31021bf47692fa9318dd108d

--- search/functions_5.js
+++ search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['lineartween_0',['linearTween',['../class_easing.html#ada86df398477fcf48f64828d23d6f5af',1,'Easing']]],
+  ['linear_5ftween_0',['linear_tween',['../class_easing.html#a47377adda881671ee8098b30e2db7466',1,'Easing']]],
   ['lookat_1',['lookAt',['../class_matrix_t.html#ac6926fac438458841ba928e77ce9f049',1,'MatrixT']]]
 ];
