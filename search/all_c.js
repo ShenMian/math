860aac1a63c489bee07e61d82000984396864d15
ShenMian <ShenMian@users.noreply.github.com>
2023-03-06 02:56:33 +0000

    deploy: e3b89cb4f00a511a0d887afd29aa60906ae4f660

--- search/all_c.js
+++ search/all_c.js
@@ -2,9 +2,9 @@ var searchData=
 [
   ['recompose_0',['recompose',['../class_matrix_t.html#a3f22ceffea18fcf0139f0057c708b3b9',1,'MatrixT']]],
   ['rotate_1',['rotate',['../class_matrix_t.html#a78384c965007f658d0547d43c7768bda',1,'MatrixT']]],
-  ['rotatex_2',['rotateX',['../class_matrix_t.html#aa1bdcf740189f69c914626357d3ee847',1,'MatrixT']]],
-  ['rotatey_3',['rotateY',['../class_matrix_t.html#a23b4ed2238797d50f2d0096dd936ef96',1,'MatrixT']]],
-  ['rotatez_4',['rotateZ',['../class_matrix_t.html#a2728fd2aac604be85051cc7c2efed388',1,'MatrixT']]],
+  ['rotate_5fx_2',['rotate_x',['../class_matrix_t.html#a101cfb5f15474a75f188cedd9879a4cf',1,'MatrixT']]],
+  ['rotate_5fy_3',['rotate_y',['../class_matrix_t.html#afaf4f26872b5111f4279477814130b5e',1,'MatrixT']]],
+  ['rotate_5fz_4',['rotate_z',['../class_matrix_t.html#ad2f38d6c2753f0de43e4e23c47f57607',1,'MatrixT']]],
   ['row_5',['row',['../class_matrix_t.html#a903a3c2bf0309b922e9a9b5d571dc1df',1,'MatrixT']]],
   ['rows_6',['rows',['../class_matrix_t.html#ae88068b21b659971332de5b63ea4b14b',1,'MatrixT']]]
 ];
