485f4592b2ad69dac3da300dd89b8314cb36019b
ShenMian <ShenMian@users.noreply.github.com>
2023-06-21 09:59:42 +0000

    deploy: b0498ee5b6c515f9026e26197b937a9464f3af03

--- search/functions_1.js
+++ search/functions_1.js
@@ -7,11 +7,11 @@ var searchData=
   ['components_4',['components',['../class_vector_t.html#a563f20e9cdbbe3c20b5dacdcc921f235',1,'VectorT']]],
   ['conjugate_5',['conjugate',['../class_quaternion_t.html#a49cc1085986c121315299949aa2f8c68',1,'QuaternionT']]],
   ['contains_6',['contains',['../class_a_a_b_b.html#a3bf5889b8fc8d80befdb92be51b1c788',1,'AABB::contains(const Vectorf&lt; N &gt; &amp;point) const'],['../class_a_a_b_b.html#a7f0957e12a86af6a494339da3e665a74',1,'AABB::contains(const AABB &amp;aabb) const']]],
-  ['createrotation_7',['createRotation',['../class_matrix_t.html#ab86629c669f9bfee09901aa5ec170459',1,'MatrixT::createRotation(const QuaternionT&lt; T &gt; &amp;q)'],['../class_matrix_t.html#aad54e65bc8faa2e8dcb11fe9c530e54a',1,'MatrixT::createRotation(float angle, Vector3T&lt; T &gt; axis)']]],
-  ['createrotationx_8',['createRotationX',['../class_matrix_t.html#a2caf5c73121a2307d1857dc050de2c36',1,'MatrixT']]],
-  ['createrotationy_9',['createRotationY',['../class_matrix_t.html#aac008c7d0cab4ef61e9eb45a17aa890d',1,'MatrixT']]],
-  ['createrotationz_10',['createRotationZ',['../class_matrix_t.html#a9283ee089eda3ba9bf23e1e58173ada0',1,'MatrixT']]],
-  ['createscale_11',['createScale',['../class_matrix_t.html#a24e3f77ac98f031bb84214c72906b30e',1,'MatrixT']]],
-  ['createtranslate_12',['createTranslate',['../class_matrix_t.html#a520bb2d5da2af238f9cb7eb75ae11d17',1,'MatrixT']]],
+  ['create_5frotation_7',['create_rotation',['../class_matrix_t.html#a8a1be94b69a9b7088f3fbf7fe0b197e1',1,'MatrixT::create_rotation(const QuaternionT&lt; T &gt; &amp;q)'],['../class_matrix_t.html#a6e32aa6c5e3a730db204b13381d3ac98',1,'MatrixT::create_rotation(float angle, Vector3T&lt; T &gt; axis)']]],
+  ['create_5frotation_5fx_8',['create_rotation_x',['../class_matrix_t.html#ae3411f3eb7198945823ef2a70cbaf97c',1,'MatrixT']]],
+  ['create_5frotation_5fy_9',['create_rotation_y',['../class_matrix_t.html#aa72491e35b3923438edf0a841d0cdb25',1,'MatrixT']]],
+  ['create_5frotation_5fz_10',['create_rotation_z',['../class_matrix_t.html#a9e90e6b2ea046d200618cedaa5a1319d',1,'MatrixT']]],
+  ['create_5fscale_11',['create_scale',['../class_matrix_t.html#a4333b3fce09aecc1811a6748b450d1ab',1,'MatrixT']]],
+  ['create_5ftranslate_12',['create_translate',['../class_matrix_t.html#a5c3aeda91d30459809a28cf72c0d00c1',1,'MatrixT']]],
   ['cross_13',['cross',['../class_vector_t.html#a6121eef21255c08c9f78579f5b791ccd',1,'VectorT']]]
 ];
--- search/functions_5.js
+++ search/functions_5.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['linear_5ftween_0',['linear_tween',['../class_easing.html#a47377adda881671ee8098b30e2db7466',1,'Easing']]],
-  ['lookat_1',['lookAt',['../class_matrix_t.html#ac6926fac438458841ba928e77ce9f049',1,'MatrixT']]]
+  ['look_5fat_1',['look_at',['../class_matrix_t.html#a96e66431bf91db16631eaaad7b513814',1,'MatrixT']]]
 ];
